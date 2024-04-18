cbb9bc8ab87663f8aaa7d96f823225478d0b7dc7
Carl_1506 <carl.fernandes@mail.utoronto.ca>
2024-04-18 15:07:41 -0400

    regression models and editing document

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,31 +1,40 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Model
+# Author: Carl Fernandes
+# Date: 17 April 2024
+# Contact: carl.fernandes@mail.utoronto.ca
+
 
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
 
+
 #### Read data ####
 analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+analysis_data$Country <- as.factor(analysis_data$Country)
+analysis_data$Gini_Index <- as.numeric(analysis_data$Gini_Index)
+analysis_data$IHDI <- as.numeric(analysis_data$IHDI)
+
+# first model
+first_model <- lm(Happiness_score ~ Gini_Index, data = analysis_data)
+
+# Check the summary of the model to see the coefficients and statistics
+summary(first_model)
+
+
+#second_model
+second_model <- lm(Happiness_score ~ Democracy_index + Gini_Index + IHDI, data = analysis_data)
+
+# Check the summary of the model
+summary(second_model)
+
+# Optional: Diagnostic plots to check for assumptions
+# If you have a large number of countries, these plots may be less informative
+par(mfrow = c(2, 2))
+plot(second_model)
 
 
 #### Save model ####
@@ -34,4 +43,7 @@ saveRDS(
   file = "models/first_model.rds"
 )
 
-
+saveRDS(
+  second_model,
+  file = "models/second_model.rds"
+)
